c15e067a70a25a95a930eb60a1e4a2e788dffc15
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-31 12:59:54 +0100

    Added @WebMvcTest to test output of Thymeleaf template. Removed controllers from the SpringBootApplication.

--- src/main/java/top/kerstholt/springwithspringboot/SpringWithSpringBootApplication.java
+++ src/main/java/top/kerstholt/springwithspringboot/SpringWithSpringBootApplication.java
@@ -1,22 +1,7 @@
 package top.kerstholt.springwithspringboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import top.kerstholt.springwithspringboot.business.domain.RoomReservation;
-import top.kerstholt.springwithspringboot.business.service.ReservationService;
-import top.kerstholt.springwithspringboot.data.entity.Reservation;
-import top.kerstholt.springwithspringboot.data.entity.Room;
-import top.kerstholt.springwithspringboot.data.repository.ReservationRepository;
-import top.kerstholt.springwithspringboot.data.repository.RoomRepository;
-
-import java.sql.Date;
-import java.time.LocalDate;
-import java.util.List;
 
 /**
  * The @SpringBootApplication annotation
@@ -33,43 +18,4 @@ public class SpringWithSpringBootApplication {
 		SpringApplication.run(SpringWithSpringBootApplication.class, args);
 	}
 
-	// REST controller to test the RoomRepository.
-	@RestController
-	@RequestMapping("/rooms")
-	public class RoomController {
-
-		@Autowired
-		private RoomRepository roomRepository;
-
-		@GetMapping
-		public Iterable<Room> getRooms() {
-			return this.roomRepository.findAll();
-		}
-	}
-
-//	@RestController
-//	@RequestMapping("/reservations")
-//	public class ReservationsController {
-//
-//		@Autowired
-//		private ReservationRepository reservationRepository;
-//
-//		@GetMapping
-//		public Iterable<Reservation> getReservations() {
-//			return this.reservationRepository.findAll();
-//		}
-//	}
-//
-//	@RestController
-//	@RequestMapping("/reservations/{date}")
-//	public class ReservationController {
-//
-//		@Autowired
-//		private ReservationService reservationService;
-//
-//		@GetMapping
-//		public List<RoomReservation> getReservations(@PathVariable("date") String sDate) {
-//			return this.reservationService.getRoomReservations(Date.valueOf(LocalDate.parse(sDate)));
-//		}
-//	}
 }
--- src/main/java/top/kerstholt/springwithspringboot/business/domain/RoomReservation.java
+++ src/main/java/top/kerstholt/springwithspringboot/business/domain/RoomReservation.java
@@ -2,6 +2,7 @@ package top.kerstholt.springwithspringboot.business.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
