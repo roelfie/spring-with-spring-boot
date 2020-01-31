05561df1f73925b54912532e86bebb81ec8ca1c0
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-31 13:27:34 +0100

    Added a @RestController to expose our data via an API. Compare *WebController with *WebServiceController.

--- src/main/java/top/kerstholt/springwithspringboot/web/RoomReservationWebServiceController.java
+++ src/main/java/top/kerstholt/springwithspringboot/web/RoomReservationWebServiceController.java
@@ -1,35 +1,31 @@
 package top.kerstholt.springwithspringboot.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import top.kerstholt.springwithspringboot.business.domain.RoomReservation;
 import top.kerstholt.springwithspringboot.business.service.ReservationService;
 
 import java.util.Date;
 import java.util.List;
 
-@Controller
-@RequestMapping("/reservations")
-public class RoomReservationWebController {
+@RestController
+@RequestMapping("/api/reservations")
+public class RoomReservationWebServiceController {
 
     private final ReservationService reservationService;
 
     @Autowired
-    public RoomReservationWebController(ReservationService reservationService) {
+    public RoomReservationWebServiceController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
-    // http://localhost:8080/reservations?date=2020-01-01
+    // http://localhost:8080/api/reservations?date=2020-01-01
     @GetMapping
-    public String getReservations(@RequestParam(value="date", required=false) String dateString, Model model) {
+    public List<RoomReservation> getReservations(@RequestParam(value="date", required=false) String dateString) {
         Date date = DateUtils.createDateFromString(dateString);
-        List<RoomReservation> roomReservations = reservationService.getRoomReservations(date);
-        model.addAttribute("roomReservations", roomReservations);
-
-        return "reservations"; // This tells Thymeleaf to find a template named "reservations" & populate it with model.
+        return reservationService.getRoomReservations(date);
     }
 }
